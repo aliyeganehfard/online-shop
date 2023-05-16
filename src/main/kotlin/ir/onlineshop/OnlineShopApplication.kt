0ece4845646b1c4df848979c46741c3d92a057b3
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2023-05-16 15:29:54 +0330

    Initial commit

---
+++ src/main/kotlin/ir/onlineshop/OnlineShopApplication.kt
@@ -0,0 +1,11 @@
+package ir.onlineshop
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class OnlineShopApplication
+
+fun main(args: Array<String>) {
+	SpringApplication.run(OnlineShopApplication::class.java, *args)
+}
---
+++ src/test/kotlin/ir/onlineshop/OnlineShopApplicationTests.kt
@@ -0,0 +1,13 @@
+package ir.onlineshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OnlineShopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
