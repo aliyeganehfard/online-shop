1c25be8e4ed4f304aaba2e04dd2ba8be6c17715a
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2023-05-24 20:55:35 +0330

    #30

--- src/main/kotlin/ir/onlineshop/common/dto/product/ProductResDto.kt
+++ src/main/kotlin/ir/onlineshop/common/dto/product/ProductResDto.kt
@@ -1,10 +1,13 @@
 package ir.onlineshop.common.dto.product
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import ir.onlineshop.common.dto.productProperties.ProductPropertiesDto
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class ProductResDto(
-    var name: String = "",
-    var description: String = "",
-    var price: Long = 0,
-    var properties: List<ProductPropertiesDto>? = null
+    var name: String? = null,
+    var description: String? = null,
+    var price: Long? = null,
+    var properties: List<ProductPropertiesDto>? = null,
+    var categoryTitle: String? = null
 )
